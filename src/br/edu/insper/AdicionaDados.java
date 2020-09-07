dc76d4bdfebbe88c1fd0867e4c03630835dbf56b
Manuel-castanares <49621895+Manuel-castanares@users.noreply.github.com>
2020-09-07 18:38:46 -0300

    Add files via upload

---
+++ src/br/edu/insper/AdicionaDados.java
@@ -0,0 +1,60 @@
+package br.edu.insper;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AdicionaDados
+ */
+@WebServlet("/AdicionaDados")
+public class AdicionaDados extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AdicionaDados() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		String nome = request.getParameter("nome");
+		String email = request.getParameter("email");
+		String curso = request.getParameter("curso");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("Nome: " + nome);
+		out.println("<br />");
+		out.println("Email: " + email);
+		out.println("<br />");
+		out.println("Curso: " + curso);
+		out.println("<br />");
+		out.println("<p>");
+		out.println("Olá " + nome + " você está inscrito no curso " + curso + "!!!");
+		out.println("</p>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
